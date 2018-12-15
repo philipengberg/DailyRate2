b092b0a3d6f28289725233801218a78afdaf3d53
Philip Engberg <philipengberg@gmail.com>
2018-12-15 12:40:21 +0100

    Initial Commit

---
+++ DailyRate2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DailyRate2
+//
+//  Created by Philip Engberg on 15/12/2018.
+//  Copyright © 2018 Simple Sense. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
